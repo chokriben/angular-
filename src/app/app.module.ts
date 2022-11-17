dcedf592468d332e8886869536d4c336642ec0d4
chokriben <stubsoft.develop@gmail.com>
2022-11-17 20:43:28 +0100

    Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule
